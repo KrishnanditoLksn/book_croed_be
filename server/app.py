5bf056d7bd19257dd7caea74965cde92ecb0f007
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-20 21:40:10 +0700

    feat:add post book

--- server/app.py
+++ server/app.py
@@ -1,15 +1,16 @@
-from flask  import Flask,jsonify
+from flask  import Flask,jsonify , request
 from flask_cors import CORS
 from utils.util import BOOKS
-# from urllib.parse import quote as url_quote
-
+import requests
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-CORS(app, resources={r'/*': {'origins': '*'}})
-
+CORS(
+    app, 
+    resources={r'/*': {'origins': '*'}}
+)
 
 
 
@@ -23,6 +24,23 @@ def getAllBooks():
         'status':'Success',
         'books' : BOOKS
     })
+
+@app.route('/books/add' , methods=['POST'])
+def postBook():
+    book_request =  request.get_json()
+    
+    BOOKS.append({
+        'title':book_request.get('title'),
+        'author':book_request.get('author'),
+        'read':False
+    })
+    
+    return jsonify({
+        'status':'Success',
+        'messsage':'Book Successfully Added to List'
+    })
+
+
 
 if __name__ == '__main__':
     app.run()
